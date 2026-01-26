f4e7cefa3793f893454258858e53359e2f72c1ca
Sanjay Ladumor <sanjayladumor2007@gmail.com>
2026-01-26 22:29:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,226 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+const Base_Url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdownselect = document.querySelectorAll(".select-container select")
+
+const msg = document.querySelector(".conversion")
+
+const btn = document.querySelector(".exbutton button")
+
+for (let select of dropdownselect) {
+    for (currcode in countryList) {
+        let newopt = document.createElement("option")
+        newopt.innerText = currcode
+        newopt.value = currcode
+        if (select.name === "from" && currcode === "USD") {
+            newopt.selected = "selected"
+        }
+        else if (select.name === "to" && currcode === "INR") {
+            newopt.selected = "selected"
+        }
+        select.append(newopt);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateflag(evt.target);
+    })
+}
+
+const updateflag = (element) => {
+    let currcode = element.value;
+    let countrycode = countryList[currcode];
+    let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+    element.parentElement.querySelector("img").src = newsrc;
+}
+
+btn.addEventListener("click",() => {
+    logic();
+})
+
+const logic = async() => {
+    let amount = document.querySelector(".amount input")
+    let amtval = amount.value;
+    if (amtval === "" || amtval < 1) {
+        amtval = 1;
+        amount.value = 1;
+    }
+
+    let fromv = document.querySelector(".from .select-container select");
+    let fromval = fromv.value.toLowerCase();
+
+    let tov = document.querySelector(".to .select-container select");
+    let toval = tov.value.toLowerCase();
+
+    const URL = `${Base_Url}/${fromval}.json`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromval][toval];
+    let finalamount = rate * amtval;
+    msg.innerText = `${amtval} ${fromv.value} = ${finalamount} ${tov.value}`
+}
+
+window.addEventListener("load", () => {
+    logic();
+})
